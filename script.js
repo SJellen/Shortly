17106dc5c809aa02e637055329cfe3b0dfeb4b5a
scott jellen <scottjellen@scotts-MBP.fios-router.home>
2020-03-04 17:04:28 -0500

    working on js error message

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ function toggleMenu() {
 }
 
 hamburger.addEventListener('click', toggleMenu);
+
+
+
+
+
+const submit = document.getElementById('submit')
+// const url = document.getElementById('url')
+
+submit.addEventListener("click", e => {
+    e.preventDefault();
+    checkInput();
+});
+
+
+function checkInput(e) {
+    const url = document.getElementById('url')
+    
+
+    if (url.value === '') {
+        url.classList.add('error')
+    } else {
+        url.classList.remove('error')
+    }
+}
+
+
+/* 
+function validURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
+
+ */
